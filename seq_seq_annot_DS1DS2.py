e011871a9801926324885b9cbe102c01cd74b29a
Sajad Mousavi <smousavi71@gmail.com>
2019-06-13 12:28:20 -0700

    Update seq_seq_annot_DS1DS2.py

--- seq_seq_annot_DS1DS2.py
+++ seq_seq_annot_DS1DS2.py
@@ -146,24 +146,24 @@ def build_network(inputs, dec_inputs,char2numY,n_channels=10,input_depth=280,num
                               padding='same', activation=tf.nn.relu)
 
     shape = conv3.get_shape().as_list()
-    date_input_embed = tf.reshape(conv3, (-1, max_time,shape[1]*shape[2]))
+    data_input_embed = tf.reshape(conv3, (-1, max_time,shape[1]*shape[2]))
 
     # timesteps = max_time
     #
-    # lstm_in = tf.unstack(date_input_embed, timesteps, 1)
+    # lstm_in = tf.unstack(data_input_embed, timesteps, 1)
     # lstm_size = 128
     # # Get lstm cell output
     # # Add LSTM layers
     # lstm_cell = tf.contrib.rnn.BasicLSTMCell(lstm_size)
-    # date_input_embed, states = tf.contrib.rnn.static_rnn(lstm_cell, lstm_in, dtype=tf.float32)
-    # date_input_embed = tf.stack(date_input_embed, 1)
-    # shape = date_input_embed.get_shape().as_list()
+    # data_input_embed, states = tf.contrib.rnn.static_rnn(lstm_cell, lstm_in, dtype=tf.float32)
+    # data_input_embed = tf.stack(data_input_embed, 1)
+    # shape = data_input_embed.get_shape().as_list()
 
     embed_size = 10 #128 lstm_size # shape[1]*shape[2]
 
     # Embedding layers
     output_embedding = tf.Variable(tf.random_uniform((len(char2numY), embed_size), -1.0, 1.0), name='dec_embedding')
-    date_output_embed = tf.nn.embedding_lookup(output_embedding, dec_inputs)
+    data_output_embed = tf.nn.embedding_lookup(output_embedding, dec_inputs)
 
 
     with tf.variable_scope("encoding") as encoding_scope:
@@ -171,7 +171,7 @@ def build_network(inputs, dec_inputs,char2numY,n_channels=10,input_depth=280,num
 
             # Regular approach with LSTM units
             lstm_enc = tf.contrib.rnn.LSTMCell(num_units)
-            _, last_state = tf.nn.dynamic_rnn(lstm_enc, inputs=date_input_embed, dtype=tf.float32)
+            _, last_state = tf.nn.dynamic_rnn(lstm_enc, inputs=data_input_embed, dtype=tf.float32)
 
         else:
 
@@ -182,7 +182,7 @@ def build_network(inputs, dec_inputs,char2numY,n_channels=10,input_depth=280,num
             ((enc_fw_out, enc_bw_out), (enc_fw_final, enc_bw_final)) = tf.nn.bidirectional_dynamic_rnn(
                 cell_fw=enc_fw_cell,
                 cell_bw=enc_bw_cell,
-                inputs=date_input_embed,
+                inputs=data_input_embed,
                 dtype=tf.float32)
             enc_fin_c = tf.concat((enc_fw_final.c, enc_bw_final.c), 1)
             enc_fin_h = tf.concat((enc_fw_final.h, enc_bw_final.h), 1)
@@ -194,7 +194,7 @@ def build_network(inputs, dec_inputs,char2numY,n_channels=10,input_depth=280,num
         else:
             lstm_dec = tf.contrib.rnn.LSTMCell(2 * num_units)
 
-        dec_outputs, _ = tf.nn.dynamic_rnn(lstm_dec, inputs=date_output_embed, initial_state=last_state)
+        dec_outputs, _ = tf.nn.dynamic_rnn(lstm_dec, inputs=data_output_embed, initial_state=last_state)
 
     logits = tf.layers.dense(dec_outputs, units=len(char2numY), use_bias=True)
 
